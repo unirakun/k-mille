0b14b4d68826ddb2232a9dc2273bc84445b2f5e5
Fabien JUIF <fabien.juif@gmail.com>
2018-03-13 16:15:15 +0100

    :sparkles: ui / list expenses

---
+++ ui/ui-expenses/src/screens/list/list.listeners.js
@@ -0,0 +1,14 @@
+import { when } from 'k-ramel'
+import { load, setExpenses, setPrices, submit } from './list.reactions'
+
+export default [
+  when('@@krml/LISTENERS>ADDED>list')(load),
+  when('@@http/EXPENSES>GET>ENDED')(setExpenses),
+
+  // new expense
+  when('@@http/IMAGES>POST>ENDED')(setPrices),
+  when('@@ui/ON_SUBMIT')(submit),
+  when('@@ui/ON_SEND')(() => {
+    console.log('send files')
+  }),
+]
