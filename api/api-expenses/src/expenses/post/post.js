4bba01f86ac0793d2c673ea4a7bde5a8c8dade7c
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-04-25 07:47:28 +0200

    :bug: sheet cell number

--- api/api-expenses/src/expenses/post/post.js
+++ api/api-expenses/src/expenses/post/post.js
@@ -24,6 +24,7 @@ module.exports = async (ctx) => {
           client,
           'Description', // TODO
           user,
+          'forWhom',
           price,
           taxe,
           needRefund ? 'yes' : 'no',
