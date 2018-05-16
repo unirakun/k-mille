053e1383b11e3542004f68f7c2896e1fba1dc500
Fabien JUIF <fabien.juif@gmail.com>
2018-05-16 10:34:16 +0200

    :bug: remove service worker (#39)

--- ui/ui-expenses/src/index.js
+++ ui/ui-expenses/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 import 'babel-core/register'
 import 'babel-polyfill'
 import App from './app.container'
-import registerServiceWorker from './registerServiceWorker'
+// FIXME: import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// FIXME: registerServiceWorker()
