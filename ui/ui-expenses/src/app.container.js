2fe463ef93a94b56b38529ec437fa94778629b71
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-05-16 16:32:26 +0200

    :alien: krml 0.12.0

--- ui/ui-expenses/src/app.container.js
+++ ui/ui-expenses/src/app.container.js
@@ -1,14 +1,10 @@
 import { compose } from 'recompose'
 import { provider, listen } from '@k-ramel/react'
 import App from './app'
-import createStore from './store'
+import store from './store'
 import listeners from './app.listeners'
-
-const store = createStore()
-
-const name = 'app'
 
 export default compose(
   provider(store),
-  listen(listeners, name),
+  listen(listeners, 'app'),
 )(App)
