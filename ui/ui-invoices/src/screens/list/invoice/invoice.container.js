952101988018368d55ecc078a15c57886e968a8a
Fabien JUIF <fabien.juif@gmail.com>
2018-07-13 13:55:41 +0200

    :sparkles: invoices / paid on simple invoice

--- ui/ui-invoices/src/screens/list/invoice/invoice.container.js
+++ ui/ui-invoices/src/screens/list/invoice/invoice.container.js
@@ -4,6 +4,7 @@ import Component from './invoice'
 const mapStore = (store, { id }) => ({
   ...store.ui.list.invoices.get(id),
   onRemove: () => store.dispatch({ type: '@@ui/ON_REMOVE_INVOICE', payload: id }),
+  onPaid: () => store.dispatch({ type: '@@ui/ON_PAID_INVOICE', payload: id }),
 })
 
 export default container({ mapStore })(Component)
