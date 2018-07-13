8575002b0cd333e6da356e04deda6c65d4bcc969
Fabien JUIF <fabien.juif@gmail.com>
2018-07-13 13:36:29 +0200

    :burn: invoices / src/emails

--- api/api-invoices/src/emails/post/post.js
+++
@@ -1,9 +0,0 @@
-const store = require('./store')
-const email = require('./email')
-
-module.exports = async (ctx) => {
-  const { invoice, iban } = await store(ctx)
-  await email([invoice, iban])(ctx)
-
-  ctx.response.status = 200
-}
