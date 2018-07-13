9f740ea6f8f5316d5eb68781a0092fa4d7d7cb64
Fabien JUIF <fabien.juif@gmail.com>
2018-07-13 13:55:41 +0200

    :sparkles: invoices / paid on simple invoice

--- ui/ui-invoices/src/screens/list/list.listeners.js
+++ ui/ui-invoices/src/screens/list/list.listeners.js
@@ -1,9 +1,11 @@
 import { when } from 'k-ramel'
-import { load, set, map, remove } from './list.reactions'
+import { load, set, map, remove, setPaid } from './list.reactions'
 
 export default [
   when('@@krml/LISTENERS>ADDED>list')(load),
   when('@@http/INVOICES>GET>ENDED')(set),
   when('@@krf/SET>DATA>INVOICES')(map),
+  when('@@krf/REMOVE>DATA>INVOICES')(map),
   when('@@ui/ON_REMOVE_INVOICE')(remove),
+  when('@@ui/ON_PAID_INVOICE')(setPaid),
 ]
