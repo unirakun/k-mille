85f53aacc8be83f80086e82169969e5a28724ade
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-08-20 19:55:39 +0200

    :hammer: expenses | move store definition (#90)

--- ui/ui-expenses/src/store/index.js
+++ ui/ui-expenses/src/store/index.js
@@ -1,26 +1,10 @@
-import { createStore, types } from 'k-ramel'
+import { createStore } from 'k-ramel'
+import definition from './definition'
 import listeners from './listeners'
 import drivers from './drivers'
 
 export default createStore(
-  {
-    data: {
-      profile: types.object(),
-      fileId: types.string(),
-      prices: types.array(),
-      expenses: types.keyValue(),
-      emails: types.object(),
-    },
-    ui: {
-      header: types.object({ defaultData: { title: 'envoi' } }),
-      list: types.array(),
-      price: types.number(),
-      context: types.number(),
-      needRefund: types.bool(),
-      taxe: types.number(),
-      response: types.string(),
-    },
-  },
+  definition,
   {
     listeners,
     drivers,
