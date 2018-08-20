2459d52b2b69ad6603a8d42afc388e5f335439f4
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-08-20 08:41:21 +0200

    :hammer: expenses | move store definition

---
+++ ui/ui-expenses/src/store/definition.js
@@ -0,0 +1,20 @@
+import { types } from 'k-ramel'
+
+export default {
+  data: {
+    profile: types.object(),
+    fileId: types.string(),
+    prices: types.array(),
+    expenses: types.keyValue(),
+    emails: types.object(),
+  },
+  ui: {
+    header: types.object({ defaultData: { title: 'envoi' } }),
+    list: types.array(),
+    price: types.number(),
+    context: types.number(),
+    needRefund: types.bool(),
+    taxe: types.number(),
+    response: types.string(),
+  },
+}
